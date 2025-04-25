0c49d605078eaceb3edde19888ec4893c21aad44
suleiman-moraes <suleimanmoraes@gmail.com>
2025-04-25 14:24:07 +0100

    Initial commit

---
+++ src/test/java/com/moraes/cache_service/CacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moraes.cache_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class CacheServiceApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
